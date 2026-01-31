4c2978a9be3d3bb26cda3ce480a6a41b73392a36
Elian Simonnet <eliantest20@gmail.com>
2026-01-31 23:49:08 +0100

    modification du site pour quil représente mieux la site final

--- produits.js
+++ produits.js
@@ -1,20 +1,20 @@
 // produits.js
 export const catalogue = {
     "gold": {
-        "titre": "Full Gold 18K",
+        "titre": "Grillz en or",
         "prix": "Sur devis",
-        "image": "./image/14k-white-gold-diamond-cut-grillz-model.webp",
+        "image": "./image/grillzor.jpeg",
         "description": "Or massif 18 carats avec une finition diamantée pour un éclat maximal."
     },
     "diamond": {
         "titre": "Iced Out Diamond",
         "prix": "Sur devis uniquement",
-        "image": "https://images.unsplash.com/photo-1599643478518-a784e5dc4c8f?auto=format&fit=crop&w=500",
+        "image": "./image/grillzargent.jpeg",
         "description": "Serti de diamants naturels sélectionnés pour leur pureté exceptionnelle."
     },
     "silver": {
         "titre": "Grillz serti étoile",
-        "prix": "Sur devis uniquement",
+        "prix": "50000",
         "image": "./image/grillzma.jpeg",
         "description": "Serti de diamants naturels sélectionnés pour leur pureté exceptionnelle."
     }
